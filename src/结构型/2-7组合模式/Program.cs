9999c80af0bdf3644f12257eb4ddcf3409876efa
dong <44517714+lurudong@users.noreply.github.com>
2023-05-04 21:35:46 +0800

    修改

---
+++ src/结构型/2-7组合模式/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace _2_7组合模式
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Component根节点 公司
+            //Composite 树枝 部门
+            //Leaf 树叶 员工
+
+            Component company = new DepartComposite("大黄瓜传媒有限公司");
+
+            //创建部门
+            Component dep1 = new DepartComposite("导演部");
+
+            //创建员工
+            Component boss = new Employee("大黄瓜");
+
+            //把部门添加到公司下
+            company.Add(dep1);
+
+            //把员工添加到部门下
+            dep1.Add(boss);
+
+            company.Display(3);
+            Console.ReadKey();
+        }
+    }
+
+    //定义了子类中所有共性的内容
+    public abstract class Component
+    {
+        protected string Name { get; set; }
+
+        protected Component(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Add(Component component);
+
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+    }
+
+    /// <summary>
+    /// 部门类，树枝
+    /// </summary>
+    public class DepartComposite : Component
+    {
+        //存储部门或员工
+        private readonly List<Component> _components = new List<Component>();
+        public DepartComposite(string name) : base(name)
+        {
+
+        }
+        public override void Add(Component component)
+        {
+            _components.Add(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine($"{new string('-', depth)}{Name}");
+
+            foreach (Component component in _components)
+            {
+                ///递归 就是多输出几个---
+                component.Display(depth + 4);
+            }
+        }
+
+        public override void Remove(Component component)
+        {
+            _components.Remove(component);
+        }
+    }
+
+    /// <summary>
+    /// Employee 是我们的Leaf节点，也就是树叶，树叶是无法继续添加
+    /// </summary>
+    public class Employee : Component
+    {
+        public Employee(string name) : base(name)
+        {
+
+        }
+        public override void Add(Component component)
+        {
+
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine($"{new string('-', depth)}{Name}");
+        }
+
+        public override void Remove(Component component)
+        {
+
+        }
+    }
+
+}
---
+++ src/结构型/2-8享元模式/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace _2_8享元模式
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //共享自行车
+
+            BikeFactory bikeFactory = new BikeFactory();
+            FlyWeigthBike flyWeigthBike = bikeFactory.GetBike();
+            flyWeigthBike.Ride("张三");
+            flyWeigthBike.Back("张三");
+
+            FlyWeigthBike flyWeigthBike2 = bikeFactory.GetBike();
+            flyWeigthBike2.Ride("李四");
+
+            FlyWeigthBike flyWeigthBike3 = bikeFactory.GetBike();
+            flyWeigthBike3.Ride("王五");
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class FlyWeigthBike
+    {
+        //内部状态 BikeId State 0--锁定中 1--骑行中
+        //外部状态 用户
+
+        public string BikeId { get; set; }
+
+        public int State { get; set; }
+
+        public abstract void Ride(string userName);
+        public abstract void Back(string userName);
+    }
+
+    public class YellowBike : FlyWeigthBike
+    {
+        public YellowBike(string id)
+        {
+            BikeId = id;
+        }
+        public override void Back(string userName)
+        {
+            State = 0;
+            Console.WriteLine($"用户{userName}归还了ID是{BikeId}的自行车");
+        }
+        public override void Ride(string userName)
+        {
+            State = 1;
+            Console.WriteLine($"用户{userName}正在骑行ID是{BikeId}的自行车");
+        }
+    }
+
+    public class BikeFactory
+    {
+        private readonly List<FlyWeigthBike> _bikePool = new List<FlyWeigthBike>();
+        public BikeFactory()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                _bikePool.Add(new YellowBike(i.ToString()));
+            }
+        }
+
+        public FlyWeigthBike GetBike()
+        {
+            for (int i = 0; i < _bikePool.Count; i++)
+            {
+                if (_bikePool[i].State == 0)
+                {
+                    return _bikePool[i];
+                }
+            }
+            return null!;
+        }
+    }
+}
